b84dd1ab5429fb01679c3a52be196f0eec12f679
Zakhy1 <lirchzakhar@gmail.com>
2024-09-13 15:51:26 +0700

    cors

--- backend/sheregesh/settings.py
+++ backend/sheregesh/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'rest_framework.authtoken',
+    'corsheaders',
 
     'users.apps.UsersConfig',
     'posts.apps.PostsConfig',
@@ -46,6 +47,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -138,3 +140,7 @@ AUTH_USER_MODEL = 'users.CustomUser'
 MEDIA_URL = 'media/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000"
+]
